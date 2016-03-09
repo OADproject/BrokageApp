2d422ab3fe147bd6801a9bc939c61043f360c441
Prateek Khatri <prateek.khatri90@gmail.com>
2016-03-09 14:47:45 -0800

    Added files via upload

--- GUI/FXMLDocumentController.java
+++ GUI/FXMLDocumentController.java
@@ -5,15 +5,18 @@
  */
 
 import java.net.URL;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -38,15 +41,30 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private Label LoginError;
-
+    
     @FXML
-
+    private TextField usernameSearchField;
+    
+    @FXML
     private TextArea currentMarketStockPricesArea;
 
+    @FXML
+    private TextArea userInfoTextArea;
+    @FXML
+    private TextField usernameEdit;
+    @FXML
+    private TextField passwordEdit;
+    @FXML
+    private TextField balanceEdit;
+    @FXML
+    private TextField stockNameEdit;
+    @FXML
+    private TextField stockQtyEdit;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
+    
 
     public void validateLogin(ActionEvent actionEvent) {
         LoginError.setText("");
@@ -62,9 +80,75 @@ public class FXMLDocumentController implements Initializable {
     }
 
     public void editUsedInfo(ActionEvent actionEvent) {
+        String usn = usernameEdit.getText();
+        String pwd = passwordEdit.getText();
+        double balance = Double.parseDouble(balanceEdit.getText());
+        String stkname = stockNameEdit.getText();
+        int stkqty = Integer.parseInt(stockQtyEdit.getText());
+        
+        Market m = Market.getMarket();
+        List<User> userList = m.getUserList();
+        User dispUser = null;
+        for(User u: userList)
+        {
+            Authentication a = u.getAuth();
+            if(a.getUsername().equals(usn))
+            {
+                dispUser = u;
+                break;
+            }
+        }
+        
+        if(usn.equals("") == false)
+            dispUser.getAuth().setUsername(usn);
+        if(pwd.equals("") == false)
+            dispUser.getAuth().setPassword(pwd);
+        if(balanceEdit.getText().equals("") == false)
+            dispUser.getPortfolio().setMoneyBalance(balance);
+        if(stockNameEdit.getText().equals("")==false)
+        {
+            List<Stock> userStocks = dispUser.getPortfolio().getStocks();
+            Stock temp = null;
+            for(Stock s: userStocks)
+            {
+             if(s.getStockName().equals(stkname))
+             {
+                 temp = s;
+                 break;
+             }
+            }
+            temp.setStockName(stkname);
+            if(stockQtyEdit.getText().equals("")==false)
+            {
+                temp.setStockQty(stkqty);
+            }
+        }
     }
 
     public void viewUserStocks(ActionEvent actionEvent) {
+        String usn = usernameSearchField.getText();
+        Market m = Market.getMarket();
+        List<User> userList = m.getUserList();
+        User dispUser = null;
+        for(User u: userList)
+        {
+            Authentication a = u.getAuth();
+            if(a.getUsername().equals(usn))
+            {
+                dispUser = u;
+                break;
+            }
+        }
+        String output = "Name: "+dispUser.getName()+"\n"+"Phone: "+dispUser.getPhoneNumber()+"\n"+"Address: "+dispUser.getAddress()+"\n"+"Money Balance: "+dispUser.getPortfolio().getMoneyBalance()+"\n";
+        String output2 = "=================\n";
+        String pfolio = "Stocks Owned - >\n";
+        List<Stock> userStocks = dispUser.getPortfolio().getStocks();
+        String st="";
+        for(Stock s: userStocks)
+        {
+            st = st.concat("Stock Name: "+s.getStockName()+"\nStock Qty: "+s.getStockQty()+"\n\n");
+        }
+        userInfoTextArea.setText(output+output2+pfolio+st);
     }
 
     public void viewUserBalance(ActionEvent actionEvent) {
@@ -72,7 +156,6 @@ public class FXMLDocumentController implements Initializable {
 
     public void viewCurrentStocks(ActionEvent actionEvent) {
     }
-
     public void updateMarket(ActionEvent actionEvent) {
 //        currentMarketStockPricesArea.setText("Market Started");
         Market m =  Market.getMarket();
@@ -100,3 +183,4 @@ public class FXMLDocumentController implements Initializable {
         currentMarketStockPricesArea.setText("Market Stopped");
     }
 }
+
